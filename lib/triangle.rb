6009731d729858b33733181d0b4e32973d21994e
andrew <andrewasmit@hotmail.com>
2023-01-16 21:30:41 -0600

    commit tree acdb89edf9310542697202cf4f95c89bcd064269

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  attr_reader :side1, :side2, :side3
+
+
+  def initialize(side1, side2, side3)
+    @side1=side1
+    @side2=side2
+    @side3=side3
+    @sides=[]
+    if @side1 <= 0
+      raise TriangleError
+    elsif @side2 <= 0
+      raise TriangleError
+    elsif @side3 <= 0
+      raise TriangleError
+    end
+    @sides << @side1
+    @sides << @side2
+    @sides << @side3
+  end
+
+  def kind
+    if @sides.max >= @sides.sum/2.0
+      raise TriangleError
+    elsif @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length ==2
+      :isosceles
+    else @sides.uniq.length ==3
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Your sides are crazy AF bro"
+    end
+  end
+
 end
